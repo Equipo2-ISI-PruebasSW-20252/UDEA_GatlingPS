ba5877040804ae13a2579b12a051815819b47b12
Valeria OP <valeria.ortiz2001@gmail.com>
2025-10-06 12:29:47 -0500

    H2-transfer funds: Data modified

--- src/test/scala/parabank/Data.scala
+++ src/test/scala/parabank/Data.scala
@@ -2,7 +2,7 @@ package parabank
 
 object Data{
     val url= "https://parabank.parasoft.com/parabank/services/bank"
-    val username = "john"
-    val password = "demo"
+    val username = "pruebasoft"
+    val password = "password123"
     val userid = "12212"
 }
