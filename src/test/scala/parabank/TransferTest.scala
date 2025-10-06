917d487fdb0a1a20098dafef2e28b5483fdec69c
Valeria OP <valeria.ortiz2001@gmail.com>
2025-10-06 12:48:53 -0500

    H2-transfer funds: post() modified

--- src/test/scala/parabank/TransferTest.scala
+++ src/test/scala/parabank/TransferTest.scala
@@ -20,7 +20,7 @@ class TransferTest extends Simulation{
     .exec(
       feed(feeder)
         .exec(http("Deposits funds request")
-          .post("")
+          .post("/deposit")
           .queryParam("accountId", "#{accountId}")
           .queryParam("amount", "#{amount}")
           .check(status.is(200))
